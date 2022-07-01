dd297b1e28128479ec0d3db75b90669c4b400ff8
notzilly <imnotzilly@gmail.com>
2022-07-01 00:28:40 -0300

    added route to return all characters played by member of cast

--- app/Http/Controllers/CreditController.php
+++ app/Http/Controllers/CreditController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Credit;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Collection;
 
 class CreditController extends Controller
 {
@@ -36,6 +37,32 @@ class CreditController extends Controller
         {
             return $credits;
         }
+        throw new ModelNotFoundException();
+    }
+
+     /**
+     * Returns characters of a given credit
+     * 
+     * @param  string  $id
+     * @return \Illuminate\Support\Collection
+     * 
+     * @throws ModelNotFoundException
+     */
+    public function characters($id)
+    {
+        /**
+         * @var  \Illuminate\Database\Eloquent\Collection
+         */
+        $credits = Credit::where('id', (int)$id)->get();
+
+        if($credits->count()) {
+            return collect([
+                'id' => $credits->first()->id,
+                'name' => $credits->first()->name,
+                'characters' => $credits->map->only(['title_id', 'character', 'role'])->toArray(),
+            ]);
+        }
+
         throw new ModelNotFoundException();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('credits/{id}/characters', [CreditController::class, 'characters']);
 
 Route::apiResources([
     'shows' => ShowController::class,
