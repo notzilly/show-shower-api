e4e4cc81c778ad20956a0ab12d6b2bf13508a0db
notzilly <imnotzilly@gmail.com>
2022-07-07 15:34:40 -0300

    added token controller to manage api tokens

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class TokenController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required',
+            'device_name' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+ 
+        return ['token' => $user->createToken($request->device_name)->plainTextToken];
+    }
+}
