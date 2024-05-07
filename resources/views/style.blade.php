9b389ca681956bab6afa3d032e0c021d6b1598fd
zilly <imnotzilly@gmail.com>
2024-05-07 02:16:21 -0300

    feat: add normal stylesheets and font

---
+++ resources/views/style.blade.php
@@ -0,0 +1,24 @@
+<style>
+    body {
+        background-color: #2D3A3A;
+    }
+    #container {
+        margin: 20px 50px;
+    }
+    h3 {
+        color: #248232;
+    }
+    p {
+        color: #FCFFFC;
+    }
+    .imdb-link{
+        color: #30b444;
+    }
+
+    @media(min-width: 970px) {
+        p {
+            max-width: 50vw;
+        }
+    }
+
+</style>
